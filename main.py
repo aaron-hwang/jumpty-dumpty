dec40d0d734f33cddac43b09c36de7366f9350bb
aaron-hwang <aaronhwang22@gmail.com>
2023-12-30 17:02:49 -0500

    Structure changes

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
 import pygame
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-player = pygame.Rect((300, 250, 50, 50))
+import sys
 
-run = True
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        pygame.display.set_caption("Platforming game")
+        SCREEN_WIDTH = 800
+        SCREEN_HEIGHT = 800
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
 
-while run:
-    screen.fill((0, 0, 0))
+    def run(self):
+        player = pygame.Rect((300, 250, 50, 50))
+        run = True
+        while run:
 
-    pygame.draw.rect(screen, (255, 0, 0), player)
+            pygame.display.update()
+            # Force 60 fps maximum
+            self.clock.tick(60)
+            self.screen.fill((0, 0, 0))
 
-    key = pygame.key.get_pressed()
-    if key[pygame.K_a] == True:
-        player.move_ip(-1, 0)
-    elif key[pygame.K_d] == True:
-        player.move_ip(1, 0)
-    elif key[pygame.K_w] == True:
-        player.move_ip(0, -1)
-    elif key[pygame.K_s] == True:
-        player.move_ip(0, 1)
+            pygame.draw.rect(self.screen, (255, 0, 0), player)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    pygame.display.update()
+            key = pygame.key.get_pressed()
+            if key[pygame.K_a] == True:
+                player.move_ip(-1, 0)
+            elif key[pygame.K_d] == True:
+                player.move_ip(1, 0)
+            elif key[pygame.K_w] == True:
+                player.move_ip(0, -1)
+            elif key[pygame.K_s] == True:
+                player.move_ip(0, 1)
 
-pygame.quit()
+            # Iterate over all inputs
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    pygame.quit()
+                    sys.exit()
+    
+Game().run()
