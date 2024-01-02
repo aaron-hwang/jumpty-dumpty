d779721b7adcd63a8755429d3745125f79a976bd
aaron-hwang <aaronhwang22@gmail.com>
2024-01-02 18:08:45 -0500

    Cloud

--- main.py
+++ main.py
@@ -4,17 +4,25 @@ import sys
 
 class Game:
     def __init__(self) -> None:
+
         pygame.init()
+
         pygame.display.set_caption("Platforming game")
         SCREEN_WIDTH = 800
         SCREEN_HEIGHT = 800
+
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
+        self.image = pygame.image.load('jumpty-dumpty/data/images/clouds/cloud_1.png')
+        self.image_pos = [100, 200]
 
     def run(self):
+
         player = pygame.Rect((300, 250, 50, 50))
         run = True
         while run:
+
+            self.screen.blit(self.image, self.image_pos)
 
             pygame.display.update()
             # Force 60 fps maximum
@@ -23,6 +31,7 @@ class Game:
 
             pygame.draw.rect(self.screen, (255, 0, 0), player)
 
+            # Key handling
             key = pygame.key.get_pressed()
             if key[pygame.K_a] == True:
                 player.move_ip(-1, 0)
