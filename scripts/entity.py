02c644371ccf78d5781dccff0fe73c697aba83d0
aaron-hwang <aaronhwang22@gmail.com>
2024-01-04 19:08:29 -0500

    Added x and y axis collision

--- scripts/entity.py
+++ scripts/entity.py
@@ -7,12 +7,47 @@ class PhysicsEntity:
         self.pos = list(pos)
         self.size = size
         self.velocity = [0, 0]
+        self.collisions = {'up' : False, 'down' : False, 'right' : False, 'left' : False}
     
-    def update(self, movement=(0, 0)):
-        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.movement[1])
+    def rect(self):
+        return pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
+    
+    def update(self, tilemap, movement=(0, 0)):
+        self.collisions = {'up' : False, 'down' : False, 'right' : False, 'left' : False}
+
+        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
 
         self.pos[0] += frame_movement[0]
+        # Collision x axis
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[0] > 0:
+                    entity_rect.right = rect.left
+                    self.collisions['right'] = True
+                if frame_movement[0] < 0:
+                    entity_rect.left = rect.right
+                    self.collisions['left'] = True
+                self.pos[0] = entity_rect.x
+
         self.pos[1] += frame_movement[1]
+        # Collision y axis
+        entity_rect = self.rect()
+        for rect in tilemap.physics_rects_around(self.pos):
+            if entity_rect.colliderect(rect):
+                if frame_movement[1] > 0:
+                    entity_rect.bottom = rect.top
+                    self.collisions['down'] = True
+                if frame_movement[1] < 0:
+                    entity_rect.top = rect.bottom
+                    self.collisions['up'] = True
+                self.pos[1] = entity_rect.y
+
+        # Gravity
+        self.velocity[1] = min(5, self.velocity[1] + 0.1)
+
+        if self.collisions['down'] or self.collisions['up']:
+            self.velocity[1] = 0
 
     def render(self, surface):
         surface.blit(self.game.assets['player'], self.pos)
