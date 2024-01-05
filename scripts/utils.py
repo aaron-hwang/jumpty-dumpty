27ce8c468fbc64137438f08704b9b84e4d428ddb
aaron-hwang <aaronhwang22@gmail.com>
2024-01-04 19:08:09 -0500

    Added convenience function for loading multiple images at once

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,8 +1,16 @@
 import pygame
+import os
 
-BASE_IMAGE_PATH = 'jumpty-dumpty/data/images'
+BASE_IMAGE_PATH = 'jumpty-dumpty/data/images/'
 
 def load_image(path):
     img = pygame.image.load(BASE_IMAGE_PATH + path).convert()
     img.set_colorkey((0, 0, 0))
     return img
+
+def load_images(path):
+    images = []
+    for img_name in os.listdir(BASE_IMAGE_PATH + path):
+        images.append(load_image(path + '/' + img_name))
+    
+    return images
