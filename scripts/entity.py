c99c6892070b370588626328e0803dff6daa7aa9
aaron-hwang <aaronhwang22@gmail.com>
2024-01-09 01:06:04 -0500

    finished dashing particle support

--- scripts/entity.py
+++ scripts/entity.py
@@ -125,13 +125,16 @@ class Player(PhysicsEntity):
         if abs(self.dashing) > 50:
             self.velocity[0] = abs(self.dashing) / self.dashing * 8
             if abs(self.dashing) == 51:
-                self.velocity[0] *= 0.01
-            
-            # Particles when dashing
-            angle = random.random() * math.pi * 2
-            speed = random.random() * 0.5 + 0.5
-            p_velocity = [math.cos(angle) * speed, math.sin(angle) * speed]
+                self.velocity[0] *= 0.1
+            p_velocity = [abs(self.dashing) / self.dashing * random.random() * 3, 0]
             self.game.particles.append(Particle(self.game, 'particle', self.rect().center, velocity=p_velocity, frame=random.randint(0,7)))
+        if abs(self.dashing) in (60, 50):
+            for i in range(20):
+                # Particles when dashing
+                angle = random.random() * math.pi * 2
+                speed = random.random() * 0.5 + 0.5
+                p_velocity = [math.cos(angle) * speed, math.sin(angle) * speed]
+                self.game.particles.append(Particle(self.game, 'particle', self.rect().center, velocity=p_velocity, frame=random.randint(0,7)))
         
         # Control movement after a walljump
         if self.velocity[0] > 0:
